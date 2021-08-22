86d62a03b4ef8df145857cf7849b7a209c6c7e05
Max Reiter <max.m.reiter@gmail.com>
2021-08-21 17:40:05 -0700

    add session options

--- internal/bot/reaction/session.go
+++ internal/bot/reaction/session.go
@@ -10,6 +10,32 @@ import (
 	"github.com/skwair/harmony"
 )
 
+type SessionOption func(*Session)
+
+func WithSessionReaction(r *harmony.MessageReaction) SessionOption {
+	return func(s *Session) {
+		s.Reaction = r
+	}
+}
+
+func WithSessionGuild(g *modelsx.GuildWithPrefixes) SessionOption {
+	return func(s *Session) {
+		s.Guild = g
+	}
+}
+
+func WithBot(b bot.Client) SessionOption {
+	return func(s *Session) {
+		s.Bot = b
+	}
+}
+
+func WithTx(tx *sql.Tx) SessionOption {
+	return func(s *Session) {
+		s.Tx = tx
+	}
+}
+
 type Session struct {
 	Reaction *harmony.MessageReaction
 
